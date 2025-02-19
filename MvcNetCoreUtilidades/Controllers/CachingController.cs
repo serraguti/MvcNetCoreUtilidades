b535c8e1bcc4726931d93119b83adbef688cdbd8
serraguti <pacoserranox@gmail.com>
2025-02-19 13:39:55 +0100

    Add project files.

---
+++ MvcNetCoreUtilidades/Controllers/CachingController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MvcNetCoreUtilidades.Controllers
+{
+    public class CachingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 60, 
+            Location = ResponseCacheLocation.Client)]
+        public IActionResult MemoriaDistribuida()
+        {
+            string fecha =
+                DateTime.Now.ToLongDateString() + " -- "
+                + DateTime.Now.ToLongTimeString();
+            ViewData["FECHA"] = fecha;
+            return View();
+        }
+    }
+}
